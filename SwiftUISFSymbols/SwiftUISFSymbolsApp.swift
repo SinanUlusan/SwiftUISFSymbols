60c4c3f55fccfd2553aa0a6f767c48d61a922336
SinanStatistician <SinanStatistician@users.noreply.github.com>
2021-02-07 19:18:45 +0300

    Initial Commit

---
+++ SwiftUISFSymbols/SwiftUISFSymbolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISFSymbolsApp.swift
+//  SwiftUISFSymbols
+//
+//  Created by Sinan Ulusan on 7.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISFSymbolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
